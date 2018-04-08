37b6af657f600d42a599f7cef81bc63dc025277d
root <root@mail@bnokna.ru>
2018-04-09 01:03:33 +0300

    Initial commit

---
+++ common/config/db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 18.11.2017
+ */
+return
+[
+    'components'    => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=serdar',
+            'username' => 'root',
+            'password' => '1qazxsw2'
+        ]
+    ]
+];
